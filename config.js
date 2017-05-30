455732da2be61d01d8e6e0737a8465957230e7e2
azazel696 <cifuentes_estrada@hotmail.com>
2017-05-30 01:41:40 -0500

    si

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://<dbuser>:<dbpassword>@ds157641.mlab.com:57641/heroku_8flx7x2k',
+    db: process.env.MONGODB || 'mongodb://azazel:Source22();@ds157641.mlab.com:57641/heroku_8flx7x2k',
     SECRET_TOKEN : 'miclavedetokens'
 }
