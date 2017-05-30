b7e51ffda22404af1747fdc06b010cde9caa4dfe
Luis Cifuentes <cifuentes_estrada@hotmail.com>
2017-05-30 01:14:13 -0500

    NODE Deploy #1
    VTF

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,16 @@ const auth = require('../middlewares/auth')
 const userCtrl= require('../controllers/user')
 
 const api = express.Router()
+//Bearer
+/*
+{"authorization":"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1OTJjZmE0ZDBkNzkxZDA4Yzg3N2Q1NjgiLCJleHAiOjE0OTczMjk0ODV9.y4W391oAonuwz1tchCzEcjdJwgntKeNnPtTFQyYC6_Y"}
+*/
 
 api.get('/product', productCtrl.getProducts)
 api.get('/product/:productId', productCtrl.getProduct)
-api.post('/product', productCtrl.saveProduct)
-api.put('/product/:productId', productCtrl.updateProduct)
-api.delete('/product/:productId', productCtrl.deleteProduct)
+api.post('/product',auth, productCtrl.saveProduct)
+api.put('/product/:productId',auth, productCtrl.updateProduct)
+api.delete('/product/:productId',auth, productCtrl.deleteProduct)
 
 api.post('/signup',userCtrl.signUp)
 api.post('/signin',userCtrl.signIn)
